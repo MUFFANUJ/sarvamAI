427bd39be1ac856210f736e1448ae91ca95007e4
anujkumar.singh <anujsinghhero292@gmail.com>
2025-01-09 04:03:58 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
             <Route path="/onboarding" element={<Onboarding />} />
             <Route path="/onboarding/account" element={<LoginSignUp />} />
             <Route path="/onboarding/voices" element={<EditVoice />} />
-            <Route path="*" element={<h1 className="flex justify-center items-center bg-primary-100 text-balck">Onboarding is Pending <p>
+            <Route path="*" element={<h1 className="w-screen h-screen flex justify-center items-center bg-primary-100 text-balck">Onboarding is Pending <p>
               Please Complete The Onboarding Before Moving Forward</p></h1>} />
             <Route
               path="/persononboarding"
@@ -86,7 +86,7 @@ function App() {
                   element={!logged ? <LoginSignUp /> : <LoggedIn />}
                 />
                 <Route path="/profile/referral" element={<ShareWithOthers />} />
-                <Route path="*" element={<h1 className="flex justify-center items-center bg-primary-100 text-balck">Oopss... 404 Not Found!</h1>} />
+                <Route path="*" element={<h1 className="flex w-screen h-screen justify-center items-center bg-primary-100 text-balck">Oopss... 404 Not Found!</h1>} />
               </Routes>
             </div>
           </div>
